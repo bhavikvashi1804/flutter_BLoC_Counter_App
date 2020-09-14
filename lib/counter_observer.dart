22cdfe9c5a761f298827cd4d900e7e8435b8ca06
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-14 13:48:13 +0530

    create  BLoC Observer for counter application in our application it is counter observer

---
+++ lib/counter_observer.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+
+/// counter_observer
+/// [BlocObserver] for the counter application which
+/// observes all [Cubit] state changes.
+
+class CounterObserver extends BlocObserver {
+  @override
+  void onChange(Cubit cubit, Change change) {
+    print('${cubit.runtimeType} $change');
+    super.onChange(cubit, change);
+  }
+}
