84abd2f6f25c29a6120c2e544a94e0d0522c7bc4
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-09-14 13:45:31 +0530

    Initial Commit for Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-
-import './counter_bloc.dart';
-import './simple_bloc_delegate.dart';
 void main() {
-  BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Counter App',
-      home: BlocProvider<CounterBloc>(
-        create: (context) => CounterBloc(),
-        child: MyHomePage(),
-      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
- 
-
   @override
   Widget build(BuildContext context) {
-
-    final CounterBloc counterBloc = BlocProvider.of<CounterBloc>(context);
-    //use this to call Events
-
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Counter App'),
       ),
-      body: BlocBuilder<CounterBloc, int>(
-        //used BlocBuilder to consume data Stream
-        builder: (context, count) {
-          return Center(
-            child: Text(
-              '$count',
-              style: TextStyle(fontSize: 24.0),
-            ),
-          );
-        },
+      body: Center(
+        child: Text('Counter App Home Page'),
       ),
-      floatingActionButton: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: (){
-              counterBloc.add(CounterEvent.increment);
-            },
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(width:10),
-          FloatingActionButton(
-            onPressed: (){
-              counterBloc.add(CounterEvent.decrement);
-              //to Execute Event blocobj.add(Event)
-            },
-            tooltip: 'Decrement',
-            child: Icon(Icons.remove),
-          ),
-
-        ],
-      ), 
     );
   }
 }
